d8e724c28a100c38613db333414fd4d7fe0dfec5
Ezequiel <ezequiel.neuhauser@gmail.com>
2021-12-06 00:27:57 -0300

    Arreglos en las páginas y estilos de las mismas

---
+++ index.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector(".nav-toggle-header")
+const navMenu = document.querySelector(".nav-menu")
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu-visible");
+});
+
+    if (navMenu.classList.contains("nav-menu-visible")){
+        navToggle.setAttribute("aria-label", "Cerrar manú");
+    } else{
+        navToggle.setAttribute("aria-label", "Abrir menú")
+    }
